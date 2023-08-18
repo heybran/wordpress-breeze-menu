975d29d1e9c2e818ee386b4387bdd0db503b4602
brandon <gwonzhang@gmail.com>
2023-08-18 19:50:42 +0800

    feat: inject admin js and css

--- admin/class-breeze-menu-admin.php
+++ admin/class-breeze-menu-admin.php
@@ -48,11 +48,12 @@ class Breeze_Menu_Admin
 	 * @param      string    $Breeze_Menu       The name of this plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
-	public function __construct($Breeze_Menu, $version)
-	{
+	public function __construct($Breeze_Menu, $version) {
 
 		$this->Breeze_Menu = $Breeze_Menu;
 		$this->version = $version;
+		$this->enqueue_scripts();
+		$this->enqueue_styles();
 	}
 
 	/**
@@ -60,8 +61,7 @@ class Breeze_Menu_Admin
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_styles()
-	{
+	public function enqueue_styles() {
 
 		/**
 		 * This function is provided for demonstration purposes only.
@@ -75,7 +75,7 @@ class Breeze_Menu_Admin
 		 * class.
 		 */
 
-		wp_enqueue_style($this->Breeze_Menu, plugin_dir_url(__FILE__) . 'css/breeze-menu-admin.css', array(), $this->version, 'all');
+		wp_enqueue_style($this->Breeze_Menu, plugin_dir_url(__FILE__) . 'src/css/breeze-menu-admin.css', array(), $this->version, 'all');
 	}
 
 	/**
@@ -83,8 +83,7 @@ class Breeze_Menu_Admin
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_scripts()
-	{
+	public function enqueue_scripts() {
 
 		/**
 		 * This function is provided for demonstration purposes only.
@@ -98,6 +97,16 @@ class Breeze_Menu_Admin
 		 * class.
 		 */
 
-		wp_enqueue_script($this->Breeze_Menu, plugin_dir_url(__FILE__) . 'js/breeze-menu-admin.js', array('jquery'), $this->version, false);
+		// original codes, but no need to wait for jQuery
+		// wp_enqueue_script($this->Breeze_Menu, plugin_dir_url(__FILE__) . 'js/breeze-menu-admin.js', array('jquery'), $this->version, false);
+		wp_enqueue_script($this->Breeze_Menu, plugin_dir_url(__FILE__) . 'index.js', array(), $this->version, false, 'module');
+
+		function add_module_type_attribute($tag, $handle, $src) {
+			if ($handle === 'breeze-menu-admin') {
+					$tag = str_replace('<script', '<script type="module"', $tag);
+			}
+			return $tag;
+		}
+		add_filter('script_loader_tag', 'add_module_type_attribute', 10, 3);
 	}
 }
