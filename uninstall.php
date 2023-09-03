4a8c613d627d80a5ec280d14e0f931c9447962e8
brandon <gwonzhang@gmail.com>
2023-09-03 13:55:21 +0800

    feat: delete plugin option when deleted

--- uninstall.php
+++ uninstall.php
@@ -19,13 +19,16 @@
  * For more information, see the following discussion:
  * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
  *
- * @link       http://example.com
+ * @link       https://github.com/heybran/wordpress-breeze-menu
  * @since      1.0.0
  *
- * @package    Plugin_Name
+ * @package    Breeze_Menu
  */
 
 // If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+
+// Clean up option created by plugin
+delete_option('breeze-menu-settings');
